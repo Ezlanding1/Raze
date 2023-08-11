bef51fea37f87b8110d54b1b839e18693996ab47
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2023-08-10 21:50:56 -0400

    Fix bug with parameters as literals in inline functions being passed as 'ref'

--- Src/Assembly/CodeGen/InlinedAssembler.cs
+++ Src/Assembly/CodeGen/InlinedAssembler.cs
@@ -66,7 +66,7 @@ internal class InlinedAssembler : Assembler
         }
 
         expr.internalFunction.parameters[0].stack.stackRegister = true;
-        ((Expr.StackRegister)expr.internalFunction.parameters[0].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? operand : PassByValue(operand));
+        ((Expr.StackRegister)expr.internalFunction.parameters[0].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? operand : MovToRegister(operand));
 
         foreach (var bodyExpr in expr.internalFunction.block)
         {
@@ -123,10 +123,10 @@ internal class InlinedAssembler : Assembler
         }
 
         expr.internalFunction.parameters[0].stack.stackRegister = true;
-        ((Expr.StackRegister)expr.internalFunction.parameters[0].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? operand1 : PassByValue(operand1));
+        ((Expr.StackRegister)expr.internalFunction.parameters[0].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? operand1 : MovToRegister(operand1));
 
         expr.internalFunction.parameters[1].stack.stackRegister = true;
-        ((Expr.StackRegister)expr.internalFunction.parameters[1].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[1]) ? operand2 : PassByValue(operand2));
+        ((Expr.StackRegister)expr.internalFunction.parameters[1].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[1]) ? operand2 : MovToRegister(operand2));
 
         foreach (var bodyExpr in expr.internalFunction.block)
         {
@@ -204,7 +204,7 @@ internal class InlinedAssembler : Assembler
             Instruction.Value arg = expr.arguments[i].Accept(this);
 
             expr.internalFunction.parameters[i].stack.stackRegister = true;
-            ((Expr.StackRegister)expr.internalFunction.parameters[i].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? arg : PassByValue(arg));
+            ((Expr.StackRegister)expr.internalFunction.parameters[i].stack).register = LockOperand(IsRefParameter(expr.internalFunction.parameters[0]) ? arg : MovToRegister(arg));
         }
 
         foreach (var bodyExpr in expr.internalFunction.block)
