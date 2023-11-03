26a66f4119532023a5675b3ce9c443cade184703
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2023-11-03 17:17:15 -0400

    Add RawInstruction to encode instructions that do not fit the existing patterns

---
+++ Raze/Src/Assembler/InstructionEncoding/RawInstruction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raze;
+
+public partial class Assembler
+{
+    internal partial struct Instruction
+    {
+        // Raw Instruction Byte Data. (1 byte). Required
+        // For instructions that do not match any existing pattern, and must be encoded as raw bits
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct RawInstruction : IInstruction
+        {
+            byte _data;
+
+            public RawInstruction(byte _data)
+            {
+                this._data = _data;
+            }
+
+            public byte ToByte()
+            {
+                return _data;
+            }
+        }
+    }
+}
