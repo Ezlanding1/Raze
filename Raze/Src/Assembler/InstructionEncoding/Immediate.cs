eb8dc0471dcdc2615862e1661ec26cc106f56485
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2023-11-04 21:35:13 -0400

    Add support for immediates

--- Raze/Src/Assembler/InstructionEncoding/Immediate.cs
+++ Raze/Src/Assembler/InstructionEncoding/Immediate.cs
@@ -12,16 +12,20 @@ public partial class Assembler
     internal partial struct Instruction
     {
         // Immediate Data. (1, 2, or 4 bytes). Optional
-        // NOT CURRENTLY SUPPORTED
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
         internal struct Immediate8 : IInstruction
         {
             // 1 byte immediate
             byte _data;
 
+            public Immediate8(byte _data)
+            {
+                this._data = _data;
+            }
+
             public byte ToByte()
             {
-                throw new NotImplementedException();
+                return _data;
             }
         }
 
@@ -29,6 +33,15 @@ public partial class Assembler
         {
             // 2 byte immediate
             unsafe fixed byte _data[2];
+
+            public Immediate16(params byte[] _data) 
+            {
+                unsafe
+                {
+                    this._data[0] = _data[0];
+                    this._data[1] = _data[1];
+                }
+            }
 
             public byte ToByte()
             {
@@ -40,6 +53,17 @@ public partial class Assembler
         {
             // 4 byte immediate
             unsafe fixed byte _data[4];
+
+            public Immediate32(params byte[] _data)
+            {
+                unsafe
+                {
+                    this._data[0] = _data[0];
+                    this._data[1] = _data[1];
+                    this._data[2] = _data[2];
+                    this._data[3] = _data[3];
+                }
+            }
 
             public byte ToByte()
             {
---
+++ Raze/Src/Assembler/InstructionEncoding/InstructionOpCodeImmediate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using bit = System.Byte;
+
+namespace Raze;
+
+public partial class Assembler
+{
+    internal partial struct Instruction
+    {
+        // Instruction Opcode For Immediate Operand (1 byte). Required
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        internal struct InstructionOpCodeImmediate : IInstruction
+        {
+            byte _data = 0;
+
+            // 6 bit Instruction Opcode with Immediate operand (Non-Overriding _data)
+            OpCode opCode
+            {
+                set => _data = (byte)((_data & 0x3) | (byte)value);
+            }
+            // 1 bit sign-extend mode. 0 = b is size of a, 1 = 8-bit b sign-extended to a
+            bit x
+            {
+                set => _data = (byte)((_data & 0xFD) | (value << 1));
+            }
+            // 1 bit size. 0 = 8-bit, 1 = 16-bit/32-bit
+            bit s
+            {
+                set => _data = (byte)((_data & 0xFE) | value);
+            }
+
+            // 8 bit OpCode for immediate
+            internal enum OpCode : byte
+            {
+                ADD = 0x80
+            }
+
+            internal enum SignExtend : byte
+            {
+                // Immediate is same size as operand 1
+                SameSize = 0,
+                // 1 byte operand sign-extended
+                _8BitSignExtended = 1
+            }
+
+            internal enum Size : byte
+            {
+                _8Bit = 0,
+                _16Bit = 1,
+                _32Bit = 1
+            }
+
+            public InstructionOpCodeImmediate(OpCode opCode, SignExtend x, Size s)
+            {
+                this._data = (byte)opCode;
+                this.x = (byte)x;
+                this.s = (byte)s;
+            }
+
+            public byte ToByte()
+            {
+                return _data;
+            }
+        }
+    }
+}
