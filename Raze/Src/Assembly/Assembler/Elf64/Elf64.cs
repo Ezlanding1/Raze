e198a8321c53c6a4b6fc86ecd2bb571d8193ce38
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2024-01-13 18:42:15 -0500

    Add Elf64 specification source

---
+++ Raze/Src/Assembly/Assembler/Elf64/Elf64.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raze;
+
+public partial class Linker
+{
+    internal partial class Elf64
+    {
+        // Code adapted from Elf-64 specification outlined here: https://uclibc.org/docs/elf-64-gen.pdf
+    }
+}
