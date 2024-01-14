b099984c06516385290bc7a92e3decf674b46f3c
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2024-01-13 19:58:16 -0500

    Add struct for Elf64 Relocation table

---
+++ Raze/Src/Assembly/Assembler/Elf64/RelocationTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elf64_Addr = System.UInt64;
+using Elf64_Off = System.UInt64;
+using Elf64_Half = System.UInt16;
+using Elf64_Word = System.UInt32;
+using Elf64_Sword = System.Int32;
+using Elf64_Xword = System.UInt64;
+using Elf64_Sxword = System.Int64;
+using unsigned_char = System.Byte;
+
+namespace Raze;
+
+public partial class Linker
+{
+    internal partial class Elf64
+    {
+        internal struct Elf64_Rel
+        {
+            Elf64_Addr r_offset; /* Address of reference */
+            Elf64_Xword r_info; /* Symbol index and type of relocation */
+        }
+
+        internal struct Elf64_Rela
+        {
+            Elf64_Addr r_offset; /* Address of reference */
+            Elf64_Xword r_info; /* Symbol index and type of relocation */
+            Elf64_Sxword r_addend; /* Constant part of expression */
+        }
+    }
+}
