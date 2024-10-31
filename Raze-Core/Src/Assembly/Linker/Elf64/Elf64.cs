d9cc305880b196da651d10240484c97ad9d7db74
Ezlanding1 <113404035+Ezlanding1@users.noreply.github.com>
2024-10-30 23:19:33 -0400

    Fix memory leaks with managed structs being Marshalled

--- Raze-Core/Src/Assembly/Linker/Elf64/Elf64.cs
+++ Raze-Core/Src/Assembly/Linker/Elf64/Elf64.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,14 +14,14 @@ public partial class Linker
     {
         // Code adapted from Elf-64 specification outlined here: https://uclibc.org/docs/elf-64-gen.pdf
 
-        public unsafe static ReadOnlySpan<byte> ToReadOnlySpan<T>(T t) where T : struct
+        public static ReadOnlySpan<byte> ToReadOnlySpan<T>(ref readonly T t) where T : unmanaged
         {
-            return new ReadOnlySpan<byte>(Unsafe.AsPointer(ref t), sizeof(T));
+            return MemoryMarshal.AsBytes(new ReadOnlySpan<T>(in t));
         }
 
-        public unsafe static ReadOnlySpan<byte> ToReadOnlySpan<T>(T[] t) where T : struct
+        public static ReadOnlySpan<byte> ToReadOnlySpan<T>(T[] t) where T : unmanaged
         {
-            return new ReadOnlySpan<byte>(Unsafe.AsPointer(ref t[0]), sizeof(T) * t.Length);
+            return MemoryMarshal.AsBytes((ReadOnlySpan<T>)t);
         }
     }
 }
